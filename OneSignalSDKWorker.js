65d6f233725ff00cee3e10c2c1ae9603df274dd5
MicahRyanJohnson <contact@micahryan.com>
2019-05-28 13:06:34 -0500

    Local service worker

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -2,6 +2,6 @@ importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
 
 // Set the scope to an upper path of the script location
 // Response included "Service-Worker-Allowed : /"
-navigator.serviceWorker.register("https://assets.webflowsupport.academy/OneSignalSDKWorker.js", { scope: "/" }).then(() => {
+navigator.serviceWorker.register("/OneSignalSDKWorker.js", { scope: "/" }).then(() => {
   console.log("Install succeeded as the max allowed scope was overriden to '/'.");
 });
